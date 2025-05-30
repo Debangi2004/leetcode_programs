934c3847a46ca30a10a18cd2df3cb35f5a9cf283
DEBANGI GHATAK <124430924+Debangi2004@users.noreply.github.com>
2025-05-30 07:16:30 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (13.47%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,18 +1,15 @@
 class Solution {
     public int missingNumber(int[] a) {
-        int N=a.length;
-        int hash[] = new int[N + 1]; //hash array
+        int N = a.length;
+        int xor1 = 0, xor2 = 0;
 
-        // storing the frequencies:
-        for (int i = 0; i < N; i++)
-            hash[a[i]]++;
-
-        //checking the freqencies for numbers 1 to N:
+        // XOR of array elements
         for (int i = 0; i < N; i++) {
-            if (hash[i] == 0) {
-                return i;
-            }
+            xor2 = xor2 ^ a[i];
+            xor1 = xor1 ^ (i + 1); // XOR up to N (i+1 goes from 1 to N)
         }
-        return N;
+
+        // XOR of xor1 and xor2 gives the missing number
+        return xor1 ^ xor2;
     }
 }
