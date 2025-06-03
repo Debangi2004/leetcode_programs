510242d959f33766dce628f185d70c6d185c4c2d
DEBANGI GHATAK <124430924+Debangi2004@users.noreply.github.com>
2025-06-03 16:16:23 +0530

    Time: 0 ms (100.00%) | Memory: 45.7 MB (63.78%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -0,0 +1,48 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int col0 = 1;
+
+        // Step 1: Traverse the matrix and mark 1st row & column
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    // mark i-th row
+                    matrix[i][0] = 0;
+
+                    // mark j-th column
+                    if (j != 0) {
+                        matrix[0][j] = 0;
+                    } else {
+                        col0 = 0;
+                    }
+                }
+            }
+        }
+
+        // Step 2: Set matrix[i][j] to 0 if row or column is marked
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Step 3: Handle the first row
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < m; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // Step 4: Handle the first column
+        if (col0 == 0) {
+            for (int i = 0; i < n; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
