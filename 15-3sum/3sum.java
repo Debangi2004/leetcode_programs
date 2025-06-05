738e58eb065b1488422b3fdd1cecf0ab671352cb
DEBANGI GHATAK <124430924+Debangi2004@users.noreply.github.com>
2025-06-05 12:21:28 +0530

    Time: 0 ms (100.00%) | Memory: 51.6 MB (50.73%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,33 +1,58 @@
+import java.util.AbstractList;
+
 class Solution {
     public List<List<Integer>> threeSum(int[] arr) {
-        List<List<Integer>> ans = new ArrayList<>();
-        Arrays.sort(arr);
-        int n = arr.length;
-        for (int i = 0; i < n; i++) {
-            //remove duplicates:
-            if (i != 0 && arr[i] == arr[i - 1]) continue;
+		return new AbstractList<List<Integer>>() {
 
-            //moving 2 pointers:
-            int j = i + 1;
-            int k = n - 1;
-            while (j < k) {
-                int sum = arr[i] + arr[j] + arr[k];
-                if (sum < 0) {
-                    j++;
-                } else if (sum > 0) {
-                    k--;
-                } else {
-                    List<Integer> temp = Arrays.asList(arr[i], arr[j], arr[k]);
-                    ans.add(temp);
-                    j++;
-                    k--;
-                    //skip the duplicates:
-                    while (j < k && arr[j] == arr[j - 1]) j++;
-                    while (j < k && arr[k] == arr[k + 1]) k--;
-                }
-            }
-        }
+			List<List<Integer>> ans;
 
-        return ans;
-    }
+			@Override
+			public int size() {
+				if (ans == null)
+					ans = createList(arr);
+				return ans.size();
+			}
+
+			@Override
+			public List<Integer> get(int index) {
+				if (ans == null)
+					ans = createList(arr);
+				return ans.get(index);
+			}
+		};
+	}
+
+	private List<List<Integer>> createList(int[] arr) {
+		List<List<Integer>> ans = new ArrayList<>();
+		int len = arr.length;
+
+		Arrays.sort(arr);
+		for (int i = 0; i < len - 2 && arr[i] <= 0; ++i) {
+			if (i != 0 && arr[i - 1] == arr[i])
+				continue;
+			twoSum(ans, arr, i + 1, len - 1, -arr[i]);
+		}
+		return ans;
+	}
+
+	private void twoSum(List<List<Integer>> ans, int[] arr, int left, int right, int target) {
+		while (left < right) {
+			int sum = arr[left] + arr[right];
+			if (sum < target) {
+				++left;
+				continue;
+			}
+			if (sum > target) {
+				--right;
+				continue;
+			}
+
+			ans.add(Arrays.asList(-target, arr[left++], arr[right--]));
+
+			while (left <= right && arr[left - 1] == arr[left]) {
+				++left;
+			}
+		}
+	}
+
 }
