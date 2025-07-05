61aca523a9bfbfd43036fa8ffce372fac1a2fbfb
DEBANGI GHATAK <124430924+Debangi2004@users.noreply.github.com>
2025-07-05 14:18:46 +0530

    Time: 21 ms (37.82%) | Memory: 44.9 MB (38.58%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,25 +1,36 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        int[][] dp = new int[n + 1][amount + 1];
+        int t[][] = new int[n + 1][amount + 1];
 
-        // Fill the first row with a large number (except dp[0][0])
-        for (int j = 0; j <= amount; j++) {
-            dp[0][j] = (j == 0) ? 0 : (int)1e9; // or Integer.MAX_VALUE - 1
-        }
-
-        // Fill the rest of the table
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= amount; j++) {
-                if (coins[i - 1] <= j) {
-                    dp[i][j] = Math.min(1 + dp[i][j - coins[i - 1]], dp[i - 1][j]);
-                } else {
-                    dp[i][j] = dp[i - 1][j];
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < amount + 1; j++) {
+                if (i == 0) {
+                    t[i][j] = (int) 1e9;  // large value
+                }
+                if (j == 0) {
+                    t[i][j] = 0;
                 }
             }
         }
 
-        // If unreachable, return -1
-        return (dp[n][amount] >= (int)1e9) ? -1 : dp[n][amount];
+        for (int j = 1; j < amount + 1; j++) {
+            if (j % coins[0] == 0)
+                t[1][j] = j / coins[0];
+            else
+                t[1][j] = (int) 1e9;
+        }
+
+        for (int i = 2; i < n + 1; i++) {
+            for (int j = 1; j < amount + 1; j++) {
+                if (coins[i - 1] <= j) {
+                    t[i][j] = Math.min(1 + t[i][j - coins[i - 1]], t[i - 1][j]);
+                } else {
+                    t[i][j] = t[i - 1][j];
+                }
+            }
+        }
+
+        return (t[n][amount] >= (int) 1e9) ? -1 : t[n][amount];
     }
 }
