a4b446d292d32ddb8b9d7d347359c18dae1e2497
DEBANGI GHATAK <124430924+Debangi2004@users.noreply.github.com>
2025-07-06 11:50:24 +0530

    Time: 72 ms (34.86%) | Memory: 46.8 MB (58.29%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,17 +1,41 @@
 class Solution {
-    public boolean canPartition(int[] nums) {
-        int totalSum = 0;
-        for (int num : nums) totalSum += num;
-        if (totalSum % 2 != 0) return false;
-        int targetSum = totalSum / 2;
-        boolean[] dp = new boolean[targetSum + 1];
-        dp[0] = true;
-        for (int num : nums) {
-            for (int currSum = targetSum; currSum >= num; currSum--) {
-                dp[currSum] = dp[currSum] || dp[currSum - num];
-                if (dp[targetSum]) return true;
+    public boolean PartitionSubsetSum(int[] nums, int n, int target) {
+        boolean t[][] = new boolean[n+1][target+1];
+        
+        for(int i =0;i<n+1;i++){
+            for(int j = 0;j<target+1;j++){
+                if(i==0){
+                    t[i][j]=false;
+                }
+                if(j==0){
+                    t[i][j]=true;
+                }
             }
         }
-        return dp[targetSum];
+        
+        for(int i =1;i<n+1;i++){
+            for(int j = 1;j<target+1;j++){
+                if(nums[i-1]<=j){
+                    t[i][j]=t[i-1][j] || t[i-1][j-nums[i-1]];
+                }
+                else{
+                    t[i][j]=t[i-1][j];
+                }
+            }
+        }
+        
+        return t[n][target];
+    }
+
+    public boolean canPartition(int[] nums) {
+        int sum=0;
+        for(int i = 0;i<nums.length;i++){
+            sum = sum + nums[i];
+        }
+        if(sum % 2 !=0)
+        {
+            return false;
+        }
+        return PartitionSubsetSum(nums,nums.length,sum/2); 
     }
 }
