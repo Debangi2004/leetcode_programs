724a429a3a2cd09a1435389c6807afe8a81b9361
DEBANGI GHATAK <124430924+Debangi2004@users.noreply.github.com>
2025-07-20 16:27:57 +0530

    Time: 2 ms (73.70%) | Memory: 53.7 MB (8.13%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,29 +1,33 @@
-
-
-public class Solution {
-    public static int majorityElement(int []v) {
-        //size of the given array:
-        int n = v.length;
-
-        for (int i = 0; i < n; i++) {
-            //selected element is v[i]
-            int cnt = 0;
-            for (int j = 0; j < n; j++) {
-                // counting the frequency of v[i]
-                if (v[j] == v[i]) {
-                    cnt++;
-                }
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count=0;
+        int element =0;
+        int n = nums.length;
+        for(int i =0;i<n;i++)
+        {
+            if(count ==0 )
+            {
+                count=1;
+                element = nums[i];
             }
-
-            // check if frquency is greater than n/2:
-            if (cnt > (n / 2))
-                return v[i];
+            else if(nums[i] == element)
+            {
+                count++;
+            }
+            else{
+                count--;
+            }
         }
-
-        return -1;
+        count =0;
+        for(int i =0;i<n;i++)
+        {
+            if(nums[i] == element)
+            {
+                count++;
+            }
+        }
+        if(count>n/2)
+            return element;
+        return -1; 
     }
-
-
-} 
-
-
+}
