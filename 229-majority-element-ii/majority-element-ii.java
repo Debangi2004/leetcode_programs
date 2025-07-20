967f0188437e52569358629a7bebb81baac61a8e
DEBANGI GHATAK <124430924+Debangi2004@users.noreply.github.com>
2025-07-20 16:58:02 +0530

    Time: 2 ms (99.80%) | Memory: 50.5 MB (41.47%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -2,20 +2,39 @@ import java.util.*;
 
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        Map<Integer, Integer> freqMap = new HashMap<>();
-        List<Integer> result = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
         int n = nums.length;
+        int element1=0,element2=0;
+        int count1=0,count2=0;
 
-        for (int num : nums) {
-            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
-        }
-
-        for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
-            if (entry.getValue() > n / 3) {
-                result.add(entry.getKey());
+        for(int i =0;i<n;i++){
+            if(count1 == 0 && nums[i] != element2){
+                count1=1;
+                element1 = nums[i];
+            }
+            else if(count2 == 0 && nums[i] != element1){
+                count2=1;
+                element2 = nums[i];
+            }
+            else if(nums[i] == element1) count1++;
+            else if(nums[i] == element2) count2++;
+            else{
+                count1--;
+                count2--;
             }
         }
 
-        return result;
+        count1=0;
+        count2=0;
+
+        for(int i=0;i<n;i++){
+            if(nums[i] == element1) count1++;
+            else if(nums[i] == element2) count2++;
+        }
+
+        if(count1>n/3) ans.add(element1);
+        if(count2>n/3) ans.add(element2);
+
+        return ans;
     }
 }
