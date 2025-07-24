e070b1bb27b84b15a9f28d3228b767e069461623
DEBANGI GHATAK <124430924+Debangi2004@users.noreply.github.com>
2025-07-24 12:08:15 +0530

    Time: 14 ms (87.81%) | Memory: 44.9 MB (38.85%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,37 +1,28 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        int t[][] = new int[n + 1][amount + 1];
-
-        for (int i = 0; i < n + 1; i++) {
-            for (int j = 0; j < amount + 1; j++) {
-                if (i == 0) {
-           
-                    t[i][j] = (int) 1e9;  // large value
-                }
-                if (j == 0) {
-                    t[i][j] = 0;
-                }
-            }
+        int t[][] = new int[n+1][amount+1];
+        for(int i=0;i<n+1;i++){
+            t[i][0]=0;
         }
-
-        for (int j = 1; j < amount + 1; j++) {
-            if (j % coins[0] == 0)
-                t[1][j] = j / coins[0];
-            else
-                t[1][j] = (int) 1e9;
+        for(int j=0;j<amount+1;j++){
+            t[0][j]=0;
         }
-
-        for (int i = 2; i < n + 1; i++) {
-            for (int j = 1; j < amount + 1; j++) {
-                if (coins[i - 1] <= j) {
-                    t[i][j] = Math.min(1 + t[i][j - coins[i - 1]], t[i - 1][j]);
-                } else {
-                    t[i][j] = t[i - 1][j];
+        for(int j=1;j<amount+1;j++){
+            if(j%coins[0]==0) t[1][j]=j/coins[0];
+            else  t[1][j]=Integer.MAX_VALUE-1;
+        }
+        for(int i=2;i<n+1;i++){
+            for(int j=1; j<amount+1 ; j++){
+                if(coins[i-1]<=j){
+                    t[i][j]=Math.min(1+t[i][j-coins[i-1]],t[i-1][j]);
+                }
+                else{
+                    t[i][j]=t[i-1][j];
                 }
             }
         }
-
-        return (t[n][amount] >= (int) 1e9) ? -1 : t[n][amount];
+        return t[n][amount]!=Integer.MAX_VALUE-1?t[n][amount]:-1;
     }
 }
+        
