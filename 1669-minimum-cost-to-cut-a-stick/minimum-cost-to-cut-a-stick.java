7c2530c92d4e0a8da2ffdb28bc0f8e8f8ddce0b9
DEBANGI GHATAK <124430924+Debangi2004@users.noreply.github.com>
2025-07-24 11:36:00 +0530

    Time: 22 ms (59.24%) | Memory: 42.6 MB (57.57%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
@@ -4,34 +4,29 @@ class Solution {
     public int minCost(int n, int[] cuts) {
         int m = cuts.length;
         Arrays.sort(cuts);
-
-        // Add 0 and n
-        int[] newCuts = new int[m + 2];
-        newCuts[0] = 0;
-        newCuts[m + 1] = n;
-        for (int i = 0; i < m; i++) {
-            newCuts[i + 1] = cuts[i];
+        int[] newcuts= new int [m+2];
+        newcuts[0]=0;
+        newcuts[m+1]=n;
+        for(int i = 0 ;i<m;i++){
+            newcuts[i+1]=cuts[i];
         }
 
-        // dp[i][j] = min cost to cut between cuts[i] and cuts[j]
-        int[][] dp = new int[m + 2][m + 2];
-        for (int[] row : dp)
-            Arrays.fill(row, -1);
-
-        return solve(0, m + 1, newCuts, dp);
+        int[][] dp= new int[m+2][m+2];
+        for(int[] row: dp){
+            Arrays.fill(row,-1);
+        }
+        return solve(0,m+1,newcuts,dp);
     }
+    public int solve(int i,int j , int[] newCuts, int[][]dp){
+        if(i+1 ==  j) return 0;
+        if(dp[i][j]!= -1) return dp[i][j];
 
-    private int solve(int i, int j, int[] cuts, int[][] dp) {
-        if (i + 1 == j) return 0; // No cuts possible
+        int mini = Integer.MAX_VALUE;
 
-        if (dp[i][j] != -1) return dp[i][j];
-
-        int min = Integer.MAX_VALUE;
-        for (int k = i + 1; k < j; k++) {
-            int cost = (cuts[j] - cuts[i]) + solve(i, k, cuts, dp) + solve(k, j, cuts, dp);
-            min = Math.min(min, cost);
+        for(int k=i+1;k<j;k++){
+            int cost = newCuts[j]-newCuts[i]+solve(i,k,newCuts,dp)+solve(k,j,newCuts,dp);
+            mini= Math.min(mini,cost);
         }
-
-        return dp[i][j] = min;
+        return dp[i][j]=mini;
     }
 }
