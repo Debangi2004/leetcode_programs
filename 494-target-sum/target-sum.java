49369ad7d098ad4582077458bcae21ae40b3a2db
DEBANGI GHATAK <124430924+Debangi2004@users.noreply.github.com>
2025-07-25 09:03:38 +0530

    Time: 6 ms (73.95%) | Memory: 44.4 MB (36.48%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -2,8 +2,6 @@ class Solution {
     public int count(int[] nums, int n, int target) {
         int[][] t = new int[n + 1][target + 1];
 
-        for (int j = 0; j < target + 1; j++)
-            t[0][j] = 0;
 
         for (int i = 0; i < n + 1; i++)
             t[i][0] = 1; 
