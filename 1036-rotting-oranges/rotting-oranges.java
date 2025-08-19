745e8c6841590a8a755cfc84a68a8943e2cc7948
DEBANGI GHATAK <124430924+Debangi2004@users.noreply.github.com>
2025-08-19 19:43:02 +0530

    Time: 2 ms (66.63%) | Memory: 42.7 MB (29.25%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,64 +1,50 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
+        if(grid.length==0 || grid==null) return 0;
         int m = grid.length;
         int n = grid[0].length;
-        int countOranges=0;
 
-        if(n==0||m==0)
-        {
-            return -1;
-        }
-        
-        Queue<int[]> q =new LinkedList<>();
-        for(int i =0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(grid[i][j] != 0)
-                {
-                    countOranges++;
-                    if(grid[i][j] == 2)
-                    {
-                        q.add(new int[]{i,j});
-                    }
+        Queue<int[]> q = new LinkedList<>();
+        int countFresh=0;
+        int countMin=0;
+        int count=0;
+
+        for(int i =0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1){
+                    countFresh++;
+                }
+                if(grid[i][j]==2){
+                    q.add(new int[]{i,j});
                 }
             }
         }
-        if(countOranges == 0)
-        {
-            return 0;
-        }
-        int countMin=0;
-        int count =0;
-        int dr[]={0,1,-1,0};
-        int dc[]={1,0,0,-1};
-        while(!q.isEmpty())
-        {
-            int s = q.size();
-            count=count+s;
-            for(int i =0;i<s;i++)
-            {
-                int point[] = q.poll();
+
+        if(countFresh == 0) return 0;
+
+        
+        while(!q.isEmpty()){
+            int size=q.size();
+            //count+=size;
+            for(int i=0;i<size;i++){
+                int point[]=q.poll();
                 int row=point[0];
                 int col=point[1];
-                for(int j=0;j<4;j++)
-                {
-                    int nr = row+dr[j];
-                    int nc = col+dc[j];
-
-                    if(nr<0 || nc<0 || nr>=m || nc>=n || grid[nr][nc]==2 || grid[nr][nc]==0)
+                int dr[]={1,0,-1,0};
+                int dc[]={0,1,0,-1};
+                for(int j=0;j<4;j++){
+                    int nr=row+dr[j];
+                    int nc=col+dc[j];
+                    if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]==1)
                     {
-                        continue;
+                        grid[nr][nc]=2;
+                        q.add(new int[]{nr,nc});
+                        count++;
                     }
-                    grid[nr][nc]=2;
-                    q.add(new int[]{nr,nc});
                 }
             }
-            if(q.size()!=0)
-            {
-                countMin++;
-            }
+            countMin++;
         }
-        return countOranges==count?countMin:-1;
+        return countFresh==count?countMin-1:-1;
     }
 }
