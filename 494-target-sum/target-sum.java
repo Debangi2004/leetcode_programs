6337d52732a695272a0fd248a07e2625b69c9f9c
DEBANGI GHATAK <124430924+Debangi2004@users.noreply.github.com>
2025-09-01 08:12:05 +0530

    Time: 5 ms (83.36%) | Memory: 44.2 MB (39.91%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,33 +1,32 @@
 class Solution {
-    public int count(int[] nums, int n, int target) {
-        int[][] t = new int[n + 1][target + 1];
-
-
-        for (int i = 0; i < n + 1; i++)
-            t[i][0] = 1; 
-        
-        for (int i = 1; i < n + 1; i++) {
-            for (int j = 0; j < target + 1; j++) {
-                if (nums[i - 1] <= j)
-                    t[i][j] = t[i - 1][j - nums[i - 1]] + t[i - 1][j];
-                else
-                    t[i][j] = t[i - 1][j];
-            }
-        }
-
-        return t[n][target];
-    }
-
     public int findTargetSumWays(int[] nums, int target) {
-        int sum = 0;
-        for (int i =0;i<nums.length;i++)
-            sum += nums[i];
+        int n =nums.length;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+        }
+        if (sum < Math.abs(target) || (sum + target) % 2 != 0) return 0;
 
-        if ((sum + target) % 2 != 0 || (sum + target) < 0)
-            return 0;
 
-        int s1 = (sum + target) / 2;
-
-        return count(nums, nums.length, s1);
+        int subsetSum=(sum+target)/2;
+        return countSubsets(nums,subsetSum); 
+    }
+    private int countSubsets(int[] nums, int target){
+        int n =nums.length;
+        int t[][]=new int[n+1][target+1];
+        for(int i=0;i<n;i++){
+            t[i][0]=1;
+        }
+        for(int i =1;i<n+1;i++){
+            for(int j =0;j<target+1;j++){
+                if(nums[i-1]<=j){
+                    t[i][j]=t[i-1][j]+t[i-1][j-nums[i-1]];
+                }
+                else{
+                    t[i][j]=t[i-1][j];
+                }
+            }
+        } 
+        return t[n][target];
     }
 }
