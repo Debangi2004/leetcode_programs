196fb4439adc27c1d9c141d13f6a603e10eb4e26
DEBANGI GHATAK <124430924+Debangi2004@users.noreply.github.com>
2025-09-10 13:32:39 +0530

    Time: 17 ms (45.83%) | Memory: 45.1 MB (30.65%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -13,7 +13,7 @@ class Solution {
             if(j% coins[0]==0) t[1][j]=j/coins[0];
             else t[1][j]=Integer.MAX_VALUE-1;
         }
-        for(int i=1;i<n+1;i++){
+        for(int i=2;i<n+1;i++){
             for(int j=1;j<amount+1;j++){
                 if(coins[i-1]<=j){
                     t[i][j]=Math.min(1+t[i][j-coins[i-1]],t[i-1][j]);
