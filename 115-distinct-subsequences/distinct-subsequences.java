fc99264a5fb7d304362904ede291576f39cdf450
DEBANGI GHATAK <124430924+Debangi2004@users.noreply.github.com>
2025-09-12 13:26:28 +0530

    Time: 17 ms (84.72%) | Memory: 51.2 MB (65.74%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numDistinct(String text1, String text2) {
+        int n = text1.length();
+        int m = text2.length();      
+        
+        int[][] t = new int[n+1][m+1];
+        
+        for(int i=0;i<n+1;i++){
+            t[i][0]=1;
+        }
+        for(int j=1;j<m+1;j++){
+            t[0][j]=0;
+        }
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(text1.charAt(i-1)==text2.charAt(j-1)) {
+                    t[i][j]=t[i-1][j]+t[i-1][j-1];
+                }
+                else{
+                    t[i][j]=t[i-1][j];
+                }
+            }
+        }
+        return  t[n][m];
+    }
+}
