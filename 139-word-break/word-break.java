edcd8c59148fc22941fc86df24b5a4239c92867c
DEBANGI GHATAK <124430924+Debangi2004@users.noreply.github.com>
2026-01-01 21:18:21 +0530

    Time: 1 ms (99.62%) | Memory: 43.3 MB (79.51%) - LeetSync

---
+++ 139-word-break/word-break.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String> wordSet=new HashSet<>(wordDict);
+        
+        int maxLenn=0;
+        for(String word : wordDict){
+            maxLenn=Math.max(maxLenn,word.length()); 
+        }
+
+        int n=s.length();
+        boolean dp[]= new boolean[n+1];
+        dp[0]=true;
+
+        for(int i=1;i<n+1;i++){
+            for(int j=i-1;j>=Math.max(0,i-maxLenn);j--){
+                if(dp[j] && wordSet.contains(s.substring(j,i))){
+                    dp[i]=true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+}
