29015727098495132362797c1b0f78373ea6f1f4
DEBANGI GHATAK <124430924+Debangi2004@users.noreply.github.com>
2026-01-20 12:29:51 +0530

    Time: 2 ms (84.94%) | Memory: 48 MB (30.98%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,9 +1,10 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int finalPos=nums.length-1;
-        for(int i = finalPos-1; i >= 0; i--){
-            if(i+nums[i]>=finalPos) finalPos=i;
+        int maxIndex=0;
+        for(int i =0;i<nums.length;i++){
+            if(i>maxIndex) return false;
+            maxIndex=Math.max(maxIndex,i+nums[i]);
         }
-        return finalPos==0;
+        return true;
     }
 }
