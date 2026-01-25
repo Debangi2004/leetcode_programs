ab92ffe85f581be77f8f506f7f2bc97b7d5139d2
DEBANGI GHATAK <124430924+Debangi2004@users.noreply.github.com>
2026-01-25 11:10:52 +0530

    Time: 16 ms (66.25%) | Memory: 47.4 MB (16.02%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,9 +1,8 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n=coins.length;
-        int[][] t=new int [n+1][amount+1];
-        
-        
+        int t[][]=new int[n+1][amount+1];
+
         for(int j=0;j<amount+1;j++){
             t[0][j]=Integer.MAX_VALUE-1;
         }
@@ -11,12 +10,12 @@ class Solution {
             t[i][0]=0;
         }
         for(int j=1;j<amount+1;j++){
-            if(j% coins[0]==0) t[1][j]=j/coins[0];
+            if(j%coins[0]==0) t[1][j]=j/coins[0];
             else t[1][j]=Integer.MAX_VALUE-1;
-        } 
+        }
         for(int i=2;i<n+1;i++){
             for(int j=1;j<amount+1;j++){
-                if(coins[i-1]<=j){
+                if(j>=coins[i-1]){
                     t[i][j]=Math.min(1+t[i][j-coins[i-1]],t[i-1][j]);
                 }
                 else t[i][j]=t[i-1][j];
