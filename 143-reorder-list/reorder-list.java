dcf8b58da6312939f27791dd3afa68f1a7a63258
DEBANGI GHATAK <124430924+Debangi2004@users.noreply.github.com>
2026-02-08 08:36:06 +0530

    Time: 2 ms (88.95%) | Memory: 49.2 MB (86.70%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -1,54 +1,42 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
-    ListNode middle(ListNode head){
-        ListNode slow = head;
+    public ListNode middle(ListNode head){
         ListNode fast = head;
-        
-        while(fast.next != null && fast.next.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
+        ListNode slow = head;
+
+        while(fast.next!=null && fast.next.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
         }
         return slow;
     }
-    
-    ListNode reverse(ListNode head){
-    if(head == null || head.next == null) return head;
-
-    ListNode newHead = reverse(head.next);
-    head.next.next = head;
-    head.next = null;
-    return newHead;
-}
-
-
+    public ListNode reverse(ListNode node){
+        ListNode temp = node;
+        ListNode prev = null;
+        while(temp!=null){
+            ListNode front=temp.next;
+            temp.next=prev;
+            prev=temp;
+            temp=front;
+        }
+        return prev;
+    }
     public void reorderList(ListNode head) {
-        if(head == null || head.next == null)
-            return;
-        
+        if(head == null || head.next == null) return;
+
         ListNode mid = middle(head);
         ListNode k = reverse(mid.next);
         mid.next = null;
-        
-        ListNode c1 = head;
-        ListNode c2 = k;
-        ListNode f1 = null;
-        ListNode f2 = null;
-        
-        while(c1 != null && c2 != null){
+
+        ListNode c1= head;
+        ListNode c2= k;
+        ListNode f1= null;
+        ListNode f2= null;
+
+        while(c1!=null && c2!=null){
             f1 = c1.next;
             f2 = c2.next;
             c1.next = c2;
             c2.next = f1;
-            
             c1 = f1;
             c2 = f2;
         }
