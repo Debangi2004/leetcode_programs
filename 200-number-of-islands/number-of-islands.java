bc15309ed5db4f4e8169a16016cedcf2391e30d3
DEBANGI GHATAK <124430924+Debangi2004@users.noreply.github.com>
2026-02-08 17:24:01 +0530

    Time: 3 ms (87.77%) | Memory: 52 MB (75.66%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,35 +1,26 @@
-public class Solution {
+class Solution {
+    public int numIslands(char[][] grid) {
+        if(grid == null || grid.length ==0) return 0;
+        int count=0;
 
-int n;
-int m;
-char[][] g;
-
-public int numIslands(char[][] grid) {
-    g = grid;
-    int count = 0;
-    n = g.length;
-    if (n == 0) return 0;
-    m = g[0].length;
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < m; j++)
-        {
-            if (g[i][j] == '1') {
-                DFSMarking(i, j);
-                count++;
+        for(int i =0;i<grid.length;i++){
+            for(int j =0;j<grid[0].length;j++){
+                if(grid[i][j]=='1'){
+                    count++;
+                    dfs(grid,i,j);
+                }
             }
         }
-    }    
-    return count;
-}
+        return count;
+    }
+    private void dfs(char[][] grid, int i, int j){
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length) return;
+        if(grid[i][j]=='0') return;
+        grid[i][j]='0';
 
-private void DFSMarking(int i, int j) {
-    if (i < 0 || j < 0 || i >= n || j >= m || g[i][j] != '1') 
-        return;
-    g[i][j] = '0';
-    DFSMarking(i + 1, j);
-    DFSMarking(i - 1, j);
-    DFSMarking(i, j + 1);
-    DFSMarking(i, j - 1); 
-     
-}
+        dfs(grid,i+1,j);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1);
+    }
 }
