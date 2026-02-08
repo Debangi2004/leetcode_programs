9141eb903b5e084b254d874fd8d9b4706842a056
DEBANGI GHATAK <124430924+Debangi2004@users.noreply.github.com>
2026-02-08 16:16:00 +0530

    Time: 7 ms (61.48%) | Memory: 47.3 MB (33.06%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,53 +1,31 @@
 class Solution {
-    public boolean canFinish(int V, int[][] prerequisites) {
-        // Form a graph
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < V; i++) {
-            adj.add(new ArrayList<>());
-        }
+        for (int i=0 ; i<numCourses ; i++) adj.add(new ArrayList<>());
         int m = prerequisites.length;
-        for (int i = 0; i < m; i++) {
-            adj.get(prerequisites[i][0]).add(prerequisites[i][1]);
+        for(int i =0;i<m;i++) adj.get(prerequisites[i][0]).add(prerequisites[i][1]);
+
+        int indegree[]=new int[numCourses];
+        for(int i =0;i<numCourses;i++){
+            for(int it : adj.get(i)) indegree[it]++;
         }
 
-
-
-        int indegree[] = new int[V];
-        for (int i = 0; i < V; i++) {
-            for (int it : adj.get(i)) {
-                indegree[it]++;
-            }
+        Queue<Integer> q= new LinkedList<>();
+        for(int i =0;i<numCourses;i++){
+            if(indegree[i]==0) q.add(i);
         }
 
-
-        Queue<Integer> q = new LinkedList<Integer>();
-        for (int i = 0; i < V; i++) {
-            if (indegree[i] == 0) {
-                q.add(i);
-            }
-        }
-
-
-        List<Integer> topo = new ArrayList<Integer>();
-        // o(v + e)
-        while (!q.isEmpty()) {
-            int node = q.peek();
-
-            q.remove();
-            topo.add(node);
-            // node is in your topo sort
-            // so please remove it from the indegree
-
-            for (int it : adj.get(node)) {
+        List<Integer> topo = new ArrayList<>();
+        while(!q.isEmpty()){
+            int curr=q.poll();
+            topo.add(curr);
+            for(int it : adj.get(curr)){
                 indegree[it]--;
-                if (indegree[it] == 0) q.add(it);
+                if(indegree[it]==0) q.add(it);
             }
         }
 
-
-        if (topo.size() == V) return true;
+        if(topo.size() == numCourses) return true;
         return false;
-
     }
-
-}  
+}
