8ec74c885bca8de26d6d4064c4eaf7c9a7361e49
DEBANGI GHATAK <124430924+Debangi2004@users.noreply.github.com>
2026-02-12 15:23:04 +0530

    Time: 2 ms (88.78%) | Memory: 49.4 MB (36.67%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -1,43 +1,42 @@
 class Solution {
-    public ListNode middle(ListNode head){
-        ListNode fast = head;
-        ListNode slow = head;
-
-        while(fast.next!=null && fast.next.next!=null){
-            fast=fast.next.next;
-            slow=slow.next;
-        }
-        return slow;
-    }
-    public ListNode reverse(ListNode node){
+    private ListNode reverse(ListNode node){
         ListNode temp = node;
         ListNode prev = null;
-        while(temp!=null){
-            ListNode front=temp.next;
-            temp.next=prev;
-            prev=temp;
-            temp=front;
+        while(temp != null){
+            ListNode front = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = front;
         }
         return prev;
     }
+    private ListNode middle(ListNode node){
+        ListNode fast = node;
+        ListNode slow = node;
+        while(fast.next != null && fast.next.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
     public void reorderList(ListNode head) {
-        if(head == null || head.next == null) return;
-
         ListNode mid = middle(head);
-        ListNode k = reverse(mid.next);
+        ListNode rev = reverse(mid.next);
         mid.next = null;
 
-        ListNode c1= head;
-        ListNode c2= k;
-        ListNode f1= null;
-        ListNode f2= null;
+        ListNode c1 = head;
+        ListNode c2 = rev;
+        ListNode f1 = null;
+        ListNode f2 = null;
 
-        while(c1!=null && c2!=null){
+        while(c1 != null && c2 != null){
             f1 = c1.next;
             f2 = c2.next;
+
             c1.next = c2;
             c2.next = f1;
-            c1 = f1;
+
+            c1= f1;
             c2 = f2;
         }
     }
