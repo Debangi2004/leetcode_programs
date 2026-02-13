d21149c714f26c66532e03359f6fa264af82765c
DEBANGI GHATAK <124430924+Debangi2004@users.noreply.github.com>
2026-02-13 12:26:24 +0530

    Time: 4 ms (98.64%) | Memory: 47.2 MB (91.23%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
@@ -1,26 +1,26 @@
 class Solution {
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        if(heights == null || heights.length == 0) return null;
         List<List<Integer>> ans = new ArrayList<>();
-        if(heights == null || heights.length == 0) return ans;
 
         int m = heights.length;
         int n = heights[0].length;
 
         boolean[][] pacific = new boolean[m][n];
         boolean[][] atlantic = new boolean[m][n];
-        for(int i =0 ;i<m;i++){
-            dfs(heights,pacific,i,0,m,n);
-            dfs(heights,atlantic,i,n-1,m,n);
-        }
-        for(int j =0 ;j<n;j++){
-            dfs(heights,pacific,0,j,m,n);
-            dfs(heights,atlantic,m-1,j,m,n);
-        }
 
         for(int i =0;i<m;i++){
-            for(int j =0;j<n;j++){
-                if(pacific[i][j] && atlantic[i][j]) 
-                    ans.add(Arrays.asList(i,j));
+            dfs(heights, pacific, i, 0, m, n);
+            dfs(heights, atlantic, i, n-1, m, n);
+        }
+        for(int j =0;j<n;j++){
+            dfs(heights, pacific, 0, j, m, n);
+            dfs(heights, atlantic, m-1, j, m, n);
+        }
+
+        for (int i =0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if(pacific[i][j]&&atlantic[i][j])ans.add(Arrays.asList(i,j));
             }
         }
 
@@ -28,17 +28,16 @@ class Solution {
     }
 
     private void dfs(int[][] heights, boolean[][] ocean, int r, int c, int m , int n){
-        ocean[r][c]= true;
-        int dr[]={1,0,0,-1};
-        int dc[]={0,1,-1,0};
+        ocean[r][c] = true;
 
-        for(int i=0;i<4;i++){
-            int nr=r+dr[i];
-            int nc=c+dc[i];
+        int dr[] ={1,0,0,-1};
+        int dc[] ={0,1,-1,0};
 
-            if(nr<0 || nc<0 || nr>=m || nc>=n) continue;
-            if(ocean[nr][nc]) continue;
-            if(heights[nr][nc]<heights[r][c]) continue;
+        for(int i =0;i<4;i++){
+            int nr = r+dr[i];
+            int nc = c+dc[i];
+
+            if(nr<0 || nc<0 || nr>=m || nc>=n || ocean[nr][nc] || heights[nr][nc] < heights[r][c]) continue;
 
             dfs(heights, ocean, nr, nc, m, n);
         }
